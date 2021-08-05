7d93dbe75c09d9a18e44d1efefe40c53fb5a378d
RachMink <82296790+RachMink@users.noreply.github.com>
2021-08-05 15:01:11 -0400

    Update name.php

--- name.php
+++ name.php
@@ -1,5 +1,6 @@
 <?php
 
+    //the first two lines allow us to view any errors that might occur on the web-page
     error_reporting(E_ALL);
 
     ini_set('display_errors', 1);
@@ -7,13 +8,15 @@
     $db = new SQLite3('newDB.db');
 
     $tablename = "newTable";
-
+    
+    //queries all the information fields in the table
     $results = $db->query('SELECT name FROM '. $tablename);
 
     $data= array();
     while ($row = $results->fetchArray(SQLITE3_ASSOC)){
         array_push($data, $row);
     }
-
+    
+    //prints the JSON format of the all information from the data array which includes all the information from the database table
     echo json_encode($data);
 ?>
