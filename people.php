05f78c47517058e18655086670b44b48e8cf9781
RachMink <82296790+RachMink@users.noreply.github.com>
2021-08-05 15:06:43 -0400

    Update people.php

--- people.php
+++ people.php
@@ -1,5 +1,6 @@
 <?php
-
+    
+    //prints errors that might occur on the webpage
     error_reporting(E_ALL);
 
     ini_set('display_errors', 1);
@@ -7,13 +8,15 @@
     $db = new SQLite3('newDB.db');
 
     $tablename = "newTable";
-
+    
+    //queries everything from the table
     $results = $db->query('SELECT * FROM '. $tablename);
 
     $data= array();
     while ($row = $results->fetchArray(SQLITE3_ASSOC)){
         array_push($data, $row);
     }
-
+    
+    //prints the JSON format of the data array on the webpage
     echo json_encode($data);
 ?>
